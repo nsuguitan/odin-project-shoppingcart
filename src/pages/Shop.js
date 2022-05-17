d05673b591246bcd82248d653609d3d1634c9814
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-05-17 16:07:33 -0400

    adding some directories and files, configure basic router

---
+++ src/pages/Shop.js
@@ -0,0 +1,10 @@
+const Shop = () => {
+return(
+    <div>
+        <h1>This is the Shop</h1>
+        <img src="http://placecorgi.com/250" alt = "no corgis for you :("/>
+    </div>
+)
+};
+
+export default Shop;
